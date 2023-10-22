168cd453d5ec74fbbf8a2cad4a43720230bdc29e
Ggury <g.guryev03@gmail.com>
2023-10-22 13:40:59 +0300

    ready_to_merge

--- Monster/monster.gd
+++ Monster/monster.gd
@@ -1,11 +1,13 @@
 extends CharacterBody3D
 
+signal ate_player
+
 @export var min_speed = 10
 @export var max_speed = 18
 @export var min_radar_uses = 5
 @export var max_radar_uses = 10
 
-var radar_uses
+var radar_used
 var is_agressive = false
 
 
@@ -13,15 +15,23 @@ func _ready():
 	var player_position = get_player_position()
 	look_at_player(player_position)
 	
-	radar_uses = randi_range(min_radar_uses, max_radar_uses)
+	radar_used = randi_range(min_radar_uses, max_radar_uses)
 	
 	
 func _process(delta):
-	if radar_uses <= 0:
-		var player_position = get_player_position()
+	var player_position = get_player_position()
+	if radar_used <= 0 and not is_agressive:
 		look_at_player(player_position)
 		
-		radar_uses = randi_range(min_radar_uses, max_radar_uses)
+		radar_used = randi_range(min_radar_uses, max_radar_uses)
+	
+	if is_agressive:
+		look_at(player_position, Vector3.UP)
+		
+		velocity = Vector3.FORWARD * max_speed
+		velocity = velocity.rotated(Vector3.UP, rotation.y)
+		velocity = velocity.rotated(Vector3.RIGHT, rotation.x)
+		velocity = velocity.rotated(Vector3.FORWARD, rotation.z)
 
 
 func _physics_process(_delta):
@@ -33,7 +43,7 @@ func get_player_position():
 
 
 func look_at_player(player_position):
-	look_at_from_position(position, player_position, Vector3.UP)
+	look_at(player_position, Vector3.UP)
 	rotate_object_local(Vector3.UP, randf_range(-PI / 6, PI / 6))
 
 	var random_speed = randi_range(min_speed, max_speed)
@@ -44,10 +54,31 @@ func look_at_player(player_position):
 
 
 func _on_main_player_radar_used():
-	radar_uses -= 1
+	radar_used -= 1
 
 
 func _on_near_player_area_body_exited(body):
-	print(body.name)
 	if body.name == "MainPlayer":
 		look_at_player(body.position)
+
+
+func _on_vision_timer_timeout():
+	var overlaps = $AreaOfSight.get_overlapping_bodies()
+
+	for overlap in overlaps:
+		if overlap.name != "MainPlayer":
+			continue
+			
+		var player_position = overlap.position
+		$VisionRayCast.look_at(player_position, Vector3.UP)
+		
+		if $VisionRayCast.is_colliding():
+			var collider = $VisionRayCast.get_collider()
+			if collider.name == "MainPlayer":
+				is_agressive = true
+
+
+func _on_mouth_area_body_entered(body):
+	if body.name == "MainPlayer" and is_agressive:
+		print("Game over")
+		ate_player.emit()
